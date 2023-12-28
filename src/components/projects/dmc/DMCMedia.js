4ac4b11d3ba68be7420fb7abeb19f75b4aacc7bf
Austin Duvall <132301084+austin-duvall@users.noreply.github.com>
2023-12-27 16:30:22 -0800

    poster for videos

--- src/components/projects/dmc/DMCMedia.js
+++ src/components/projects/dmc/DMCMedia.js
@@ -7,7 +7,7 @@ export default function DMCMedia() {
       <img id='dmcDT' src='https://i.ibb.co/zrfZqBp/dmc-desktop-150.png' loading='lazy' alt='desktop view of DMC website' />
       <img id='dmcTab' src='https://i.ibb.co/YBk6wvR/dmc-tablet-150.png' loading='lazy' alt='tablet view of DMC website' />
       <video autoPlay loop muted playsInline disablePictureInPicture>
-        <source src={dmcVid} type='video/mp4' />
+        <source poster='https://i.ibb.co/5kMpdF4/portfolio-loading-poster.png' src={dmcVid} type='video/mp4' />
         Your browser does not support video
       </video>
     </section>
--- src/components/projects/wsiw/WSIWMedia.js
+++ src/components/projects/wsiw/WSIWMedia.js
@@ -6,7 +6,7 @@ export default function WSIWMedia() {
     <section className='wsiwMedia'>
       <img id='wsiwLanding' src='https://i.ibb.co/C1HTdKC/wsiw-Landing.png' loading='lazy' alt="Landing view of 'What Should I Wear?'" />
       <img id='wsiwResults' src='https://i.ibb.co/YPd79hs/wsiw-Results.png' loading='lazy' alt="Results view of 'What Should I Wear?'" />
-      <video autoPlay loop muted playsInline disablePictureInPicture>
+      <video poster='https://i.ibb.co/5kMpdF4/portfolio-loading-poster.png' autoPlay loop muted playsInline disablePictureInPicture>
         <source src={wsiwVid} type='video/mp4' />
         Your browser does not support video
       </video>
