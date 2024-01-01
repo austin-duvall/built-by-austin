f398be33b39e558cee97cd7560556ea4ae31ce80
Austin Duvall <132301084+austin-duvall@users.noreply.github.com>
2024-01-01 14:42:05 -0800

    project page Scarlett update

--- src/components/projects/scarlett/ScarlettMedia.js
+++ src/components/projects/scarlett/ScarlettMedia.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import scarlettVid from '../../../assets/scarlett/scarlettMp4.mp4';
+import scarlettScreens from '../../../assets/scarlett/scarlettScreens.png';
 
 export default function ScarlettMedia() {
   return (
     <section className='scarlettMedia'>
-      <img id='scarlettDT' loading='lazy' src='https://i.ibb.co/NsMMcpS/scarlett-desktop-36.png' alt='desktop view of Scarlett restaurant website' />
-      <img id='scarlettTab' loading='lazy' src='https://i.ibb.co/jk5RcjB/scarlett-tablet-36.png' alt='tablet view of Scarlett restaurant website' />
-      <video poster='https://i.ibb.co/5kMpdF4/portfolio-loading-poster.png' autoPlay loop muted playsInline disablePictureInPicture>
-        <source src={scarlettVid} type='video/mp4' />
-        Your browser does not support video
-      </video>
+      <img loading='lazy' src={scarlettScreens} alt='desktop, tablet, and mobile views of Scarlett website' />
     </section>
   )
 }
